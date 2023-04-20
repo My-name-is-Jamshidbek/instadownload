c874ffddb1c59612aff2cb3ed75cdc61de03a536
xjama <jamshidbekollanazarov@outlook.com>
2023-04-21 04:35:31 +0500

    chotki

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ It echoes any incoming text messages.
 """
 
 import logging
+import os
+import time
+from random import randint
+from pytube import YouTube
+import youtube_dl
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InputMediaPhoto, InputMediaVideo, InputFile, InputMedia, InlineKeyboardButton, \
     InlineKeyboardMarkup
+import instaloader
+
 from pytube import YouTube
 from random import randint
 import os
-
-from requests_ import request_tiktok
-
 
 def downloadvideoyoutube(link,itag,type,path):
     try:
@@ -32,8 +36,9 @@ def downloadvideoyoutube(link,itag,type,path):
 
 downloads = {}
 
+la = instaloader.Instaloader()
 
-API_TOKEN = '5346182572:AAHc-WGROVX-bzCkF9b2Qw8v9BgABcUVMxE'
+API_TOKEN = '5161117755:AAHVm0jShFoCKKPnlIVUa3BOdnpHPO570-M'
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -48,6 +53,13 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
+    try:
+        os.mkdir(f"videos/insta/{message.chat.id}")
+        os.mkdir(f"videos/youtube/{message.chat.id}")
+        os.mkdir(f"videos/like/{message.chat.id}")
+        os.mkdir(f"videos/tiktok/{message.chat.id}")
+    except Exception as e:
+        pass
     await message.reply("Hi!\nI'm DownloaderBot!\nSend me Instagram, Youtube, Tik-Tok video or post link and "
                         "download now!\nPowered "
                         "by: t.me/mal_un ")
@@ -62,7 +74,45 @@ async def main(message: types.Message):
     m = await message.answer('Tekshirilmoqda...')
     if "instagram" in message.text:
         await m.edit_text('Instagram post.')
-
+        try:
+            post = instaloader.Post.from_shortcode(la.context, message.text.split('/')[-2])
+            await m.edit_text('Instagram post yuklab olinmoqda...')
+            r = la.download_post(post, target=str(message.chat.id))
+            await m.delete()
+            if True:
+                caption = False
+                for fayl in os.listdir(str(message.chat.id)):
+                    if fayl[-4:] == '.mp4':
+                        media = InputFile(f"{message.chat.id}/{fayl}")
+                        await message.reply_document(media)
+                    # elif fayl[-4:] == '.jpg':
+                    #     media = InputFile(str(message.chat.id) + "/" + fayl)
+                    #     await message.reply_photo(media)
+                    elif fayl[-4:] == '.txt':
+                        with open(f"{message.chat.id}/{fayl}", 'r') as f:
+                            caption = f.read()
+                            f.close()
+                        if len(caption) > 101:
+                            caption = caption[:100] + '...'
+                if caption: await message.answer(caption)
+            # else:
+            #     await message.answer("Yuklab olishni iloji bo'lmadi!")
+        except Exception as e:
+            print(e)
+            await message.answer("Yuklab olishni iloji bo'lmadi!")
+        finally:
+            dir_path = f"{message.chat.id}"
+            if os.path.exists(dir_path):
+                for filename in os.listdir(dir_path):
+                    file_path = os.path.join(dir_path, filename)
+                    try:
+                        if os.path.isfile(file_path) or os.path.islink(file_path):
+                            os.unlink(file_path)
+                        elif os.path.isdir(file_path):
+                            os.rmdir(file_path)
+                    except Exception as _:
+                        pass
+                os.rmdir(dir_path)
     elif 'you' in message.text:
         await m.edit_text('Youtube video.')
         key = str(randint(1000000, 10000000))
@@ -90,10 +140,32 @@ async def main(message: types.Message):
             await message.reply("Yuklab olishni iloji bo'lmadi!")
     elif "likee" in message.text:
         await m.edit_text('Likee video.')
+        try:
+            ydl_opts = {
+                'outtmpl': f"videos/like/{message.chat.id}/1.mp4",
+            }
+            await m.edit_text('Likee video yuklab olinmoqda')
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([message.text])
 
+            ydl_opts = {
+                'simulate': True,
+                'quiet': True,
+            }
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                meta = ydl.extract_info(message.text, download=False)
+
+            await m.delete()
+
+            video = InputFile(f"videos/like/{message.chat.id}/1.mp4")
+            await message.answer_video(video=video, caption=meta['title'])
+        except Exception as e:
+            print(e)
+        finally:
+            os.remove(f"videos/like/{message.chat.id}/1.mp4")
     elif "tiktok" in message.text:
-        await m.edit_text('Tik-tok video.')
-        await m.edit_text(text=request_tiktok(message.text, m.chat.id))
+        pass
+
 
 @dp.callback_query_handler(text_contains='downloadvideo_')
 async def menu(call: types.CallbackQuery):
